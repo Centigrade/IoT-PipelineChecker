fb87413e749472ecac6167b53f71cd3f621a6afe
Martin Fokam Simo <martin.fokam-simo@centigrade.de>
2021-01-07 17:37:40 +0100

    put the substring from the query result only for the JSONData.

--- GitlabServerConnection.ino
+++ GitlabServerConnection.ino
@@ -24,6 +24,7 @@ String GitlabServerConnection() {
   
   Serial.print("Requesting URL: ");
   Serial.println(url);
+
   
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
@@ -31,6 +32,7 @@ String GitlabServerConnection() {
                "PRIVATE-TOKEN: " + API_AUTH_TOKEN + "\r\n" +
                "Connection: close\r\n\r\n");
   unsigned long timeout = millis();
+  // Error handling if client is not available
   while (client.available() == 0) {
     if (millis() - timeout > 5000) {
       Serial.println(">>> Client Timeout !");
@@ -41,11 +43,19 @@ String GitlabServerConnection() {
   String queryResult;
   
   // Read all the lines of the reply from server and print them to Serial
-  int i = 0;
   while(client.available()){
     queryResult = client.readStringUntil('\r');
+    
+    int indexOfJsonBeginning = queryResult.indexOf("[");
+    if(indexOfJsonBeginning != -1)
+    {
+      // only use the JSON Data from the result string as query result substring 
+      queryResult = queryResult.substring(indexOfJsonBeginning);
+      Serial.print("LastWhileLoop");
+      break;
+    }
 
-    Serial.println(queryResult);
+    Serial.print(queryResult);
   }
   
   Serial.println();
